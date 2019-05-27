efdad28b066b1a0094f17a73a29606d139b13d4e
SoyamDarshan <soyam.darshan@gmail.com>
2019-05-28 03:08:44 +0530

    development day3

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+from django.urls import path, include
+from main import views
+
+
+urlpatterns = [
+    path('about-us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
+]

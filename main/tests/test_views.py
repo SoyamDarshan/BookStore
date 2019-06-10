8894f79c91d9fd418c904be71a41936ce9abd417
SoyamDarshan <soyam.darshan@gmail.com>
2019-06-11 02:34:31 +0530

    development day6

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,6 +1,7 @@
+from decimal import Decimal
+from main import models, forms
 from django.test import TestCase
 from django.urls import reverse
-from main import forms
 
 
 class TestPage(TestCase):
@@ -22,3 +23,51 @@ class TestPage(TestCase):
         self.assertContains(response, 'BookStore')
         self.assertIsInstance(response.context["form"],
                               forms.ContactForm)
+
+    def test_product_page_returns_active(self):
+        models.Product.objects.create(
+            name="The cathedral and the bazaar",
+            slug="cathedral-bazaar",
+            price=Decimal("10.00")
+        )
+        models.Product.objects.create(
+            name="A Tale of two Cities",
+            slug="tale-two-cities",
+            price=Decimal("2.00"),
+            active=False,
+        )
+        response = self.client.get(
+            reverse("products", kwargs={"tag": "all"})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BookStore")
+        product_list = models.Product.objects.active().order_by("name")
+        self.assertEqual(
+            list(response.context["object_list"]),
+            list(product_list),
+        )
+
+    def test_product_page_filters_by_tags_and_active(self):
+        cb = models.Product.objects.create(
+            name="The cathedral and the bazaar",
+            slug="cathedral-bazaar",
+            price=Decimal("10.00")
+        )
+        cb.tags.create(name="Open source", slug="opensource")
+        models.Product.objects.create(
+            name="Microsoft Windows guide",
+            slug="microsoft-windows-guide",
+            price=Decimal("12.00")
+        )
+        response = self.client.get(
+            reverse("products", kwargs={"tag": "opensource"})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BookStore")
+        product_list = (
+            models.Product.objects.active().filter(tags__slug="opensource").order_by("name")
+        )
+        self.assertEqual(
+            list(response.context["object_list"]),
+            list(product_list),
+        )

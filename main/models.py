9fe37fe4ba1bd41dac884f2f66f17bac56137efa
SoyamDarshan <soyam.darshan@gmail.com>
2019-06-15 21:44:32 +0530

    development day8

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import (AbstractUser, BaseUserManager)
 from django.db import models
+from django import forms
 
 
 class ActiveManager(models.Manager):
@@ -80,3 +81,28 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+
+class Lead(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Address(models.Model):
+    SUPPORTED_COUNTRIES = (
+        ("uk", "United Kingdom"),
+        ("us", "United States of America"),
+        ("in",  "India")
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address1 = models.CharField("Address line 1",
+                            max_length=100)
+    address2 = models.CharField("Address line 2",
+                            max_length=100, blank=True)
+    zip_code = models.CharField("ZIP / Postal code", max_length=12)
+    city = models.CharField(max_length=60)
+    country = models.CharField(max_length=3, choices=SUPPORTED_COUNTRIES)
+
+    def __str__(self):
+        return ", ".join([self.name, self.address1, self.address2, self.zip_code, self.city, self.country])
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from django.urls import path
 from main import views, models
+from django.contrib.auth import views as auth_veiws
+from main import forms
 from django.views.generic.detail import DetailView
 
 
@@ -8,6 +10,8 @@ urlpatterns = [
     path('about-us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
     path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
+    path('signup/', views.SignupView.as_view(), name="signup"),
     path('products/<slug:tag>/', views.ProductListView.as_view(), name="products"),
     path('product/<slug:slug>/', DetailView.as_view(model=models.Product), name="product"),
+    path('login/', auth_veiws.LoginView.as_view(template_name="login.html", form_class=forms.AuthenticationForm), name="login"),
 ]
